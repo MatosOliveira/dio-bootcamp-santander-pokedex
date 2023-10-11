38f829b7d76fdf58159719502eca21740fe5900d
MatosOliveira <matosalem.oliveira@gmail.com>
2023-10-11 18:05:27 -0300

    atualizando background e paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,17 @@
 //const limit = 10;
 //const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 
 function convertPokemonToHtml(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="details">
                 <ol class="types">
-                    ${convertPokemonToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}" />
+                <img src="${pokemon.photo}" alt="${pokemon.name}" />
             </div></>
         </li>`
     
