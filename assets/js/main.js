54159642bf12148a02bca88a9feb53b807e739af
MatosOliveira <matosalem.oliveira@gmail.com>
2023-10-11 16:15:45 -0300

    atualizando arquivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+//const offset = 0;
+//const limit = 10;
+//const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToHtml(pokemon) {
     return `
@@ -21,16 +21,6 @@ function convertPokemonToHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-       
-        for (let index = 0; index < pokemons.length; index++) {
-            const pokemon = pokemons[index];
-            console.log(convertPokemonToHtml(pokemon))
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-
-        }
-    })
-    .catch((error) => console.log(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+})
